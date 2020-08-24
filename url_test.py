2d31f937e1f2595b94a4d5b77555c48dc82f51e4
priyathamganta <47489106+priyathamganta@users.noreply.github.com>
2020-08-23 18:47:48 -0700

    test

--- url_test.py
+++ url_test.py
@@ -1,6 +1,6 @@
 import requests
 
 url = "http://127.0.0.1:5000/api/change"
-params = {'file_name': '/usr/local/test.txt'}
+params = {'file_name': 'test.txt'}
 r = requests.get(url=url, params=params)
 print(r)
